301dd35d8b5db8d864de8210552cf8ab2574ef69
imjohsep@gmail.com <imjohsep@gmail.com>
2016-03-06 22:41:47 -0800

    Edit webpack to support api with proxy

--- server.js
+++ server.js
@@ -1,68 +1,42 @@
 'use script';
-var fs = require('fs');
-var path = require('path');
-var express = require('express');
-var bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const proxy = require('proxy-middleware');
+const url = require('url');
+const open = require('open');
+const bodyParser = require('body-parser');
+const config = require('./webpack.config');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
 
+
+//----------------PROXY----------------
 var app = express();
 
-// var COMMENTS_FILE = path.join(__dirname, 'comments.json');
-// var CALENDAR_FILE = path.join(__dirname, 'public/static/events/2016.json');
-
-app.set('port', (process.env.PORT || 3000));
-
-app.use('/', express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-// Additional middleware which will set headers that we need on each request.
-app.use(function(req, res, next) {
-    // Set permissive CORS header - this allows this server to be used only as
-    // an API server in conjunction with something like webpack-dev-server.
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Disable caching so we'll always get the latest comments.
-    res.setHeader('Cache-Control', 'no-cache');
-    next();
-});
+app.use('/assets', proxy(url.parse('http://localhost:8081/assets')));
 
 app.get('/api/calendar/upcoming', function(req, res) {
-  fs.readFile(CALENDAR_FILE, function(err, data) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    res.json(JSON.parse(data));
-  });
+  // fs.readFile('./2016.json', 'utf8', function (err, data) {
+  //       if (err) throw err;
+  //       res.json(JSON.parse(data));
+  //   });
+  res.json({'herp': 'derp'});
 });
 
-app.post('/api/comments', function(req, res) {
-  fs.readFile(COMMENTS_FILE, function(err, data) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    var comments = JSON.parse(data);
-    // NOTE: In a real implementation, we would likely rely on a database or
-    // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
-    // treat Date.now() as unique-enough for our purposes.
-    var newComment = {
-      id: Date.now(),
-      author: req.body.author,
-      text: req.body.text,
-    };
-    comments.push(newComment);
-    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-      res.json(comments);
-    });
-  });
+app.get('/*', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-
-app.listen(app.get('port'), function() {
-  console.log('Server started: http://localhost:' + app.get('port') + '/');
+//---------------WEBPACK---------------
+new WebpackDevServer(webpack(config))
+.listen(config.port, 'localhost', (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log('Listening at localhost:' + config.port);
+  console.log('Opening your system browser...');
+  open('http://localhost:8080');
 });
+
+app.listen(8080);
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,28 @@
 module.exports = {
+  port: 8081,
+  devtool: 'eval-source-map',
   context: __dirname + "/app",
-
   entry: {
     javascript: "./app.js",
     html: "./index.html",
   },
-
+  entry: 
+   [ 'webpack-dev-server/client?http://127.0.0.1:8081',
+     'webpack/hot/only-dev-server',
+     './src/index' ],
   output: {
-    filename: "bundle.js",
-    path: __dirname + "/dist",
+    path: __dirname + '/dist/assets',
+    filename: 'bundle.js',
+    publicPath: '/assets/' 
   },
-
+  devServer: { 
+    contentBase: './src/',
+    historyApiFallback: true,
+    hot: true,
+    port: 8081,
+    publicPath: '/assets/',
+    noInfo: false 
+  },
   module: {
     loaders: [
       { 
@@ -30,9 +42,12 @@ module.exports = {
         test: /\.json$/,
         loader: "json"
       },
+      {
+        test: /\.sass/,
+        loader: 'style-loader!css-loader!postcss-loader!sass-loader?outputStyle=expanded&indentedSyntax'
+      },
     ]
   },
-
   node: {
     console: true,
     debug: true,
