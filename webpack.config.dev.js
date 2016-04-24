f82c7f351a0d4177a3b7e4fc1abbe5a0e6e9d786
imjohsep <imjohsep@gmail.com>
2016-04-24 14:03:25 -0700

    Fix spacing in webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -44,8 +44,8 @@ module.exports = {
       loader: 'style-loader!css-loader!postcss-loader!sass-loader?outputStyle=expanded&indentedSyntax'
     },
     {
-      test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-      loader : 'file-loader'
+      test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+      loader: 'file-loader'
     }],
   },
   _hotPort: 8000,
