b615631431257b6f96c3d8923e715f64cd7d9755
Joseph Watters <jwatters@jawbone.com>
2016-05-15 12:49:43 -0700

    Add grouped response to routes

--- router.js
+++ router.js
@@ -1,4 +1,22 @@
+var _ = require('lodash')
 var Event = require('./models/event')
+
+function groupEvents(events) {
+  var grouping = {}
+
+  _.map(events, function(event) {
+    var key = event._id.date.year + '-' + event._id.date.month + '-' + event._id.date.day
+
+    if (grouping[key] === undefined) {
+        grouping[key] = []
+    }
+
+    grouping[key].push(event._id)
+  })
+
+  return grouping
+}
+
 /* Routes */
 module.exports = function (app) {
   app.get('/api/events', function (req, res) {
@@ -15,6 +33,44 @@ module.exports = function (app) {
     Event.find({occurrence_at: {$gte: now, $lte: end_range}}).sort('occurrence_at').limit(5).exec(function (err, events) {
       if (err) res.send([])
       res.send(events)
+    })
+  })
+
+  app.get('/api/events/grouped', function (req, res) {
+    Event.aggregate({
+      $group: {
+        _id: {
+          date: {
+            year: {$year: "$occurrence_at"},
+            month: {$month: "$occurrence_at"},
+            day: {$dayOfMonth: "$occurrence_at"}
+          },
+          occurrence_at: "$occurrence_at",
+          description: "$description",
+          uid: "$_id"
+        },
+      }
+    }, {
+      $sort: {
+        "_id.date.year": 1,
+        "_id.date.month": 1,
+        "_id.date.day": 1,
+      }
+    }).exec(function (err, events) {
+      if (err) res.send({'error': err})
+
+      var groupedEvents = {}
+      _.map(events, function(event) {
+        var key = event._id.date.year + '-' + event._id.date.month + '-' + event._id.date.day
+
+        if (groupedEvents[key] === undefined) {
+            groupedEvents[key] = []
+        }
+
+        groupedEvents[key].push(event._id)
+      })
+
+      res.send(groupedEvents)
     })
   })
 }
