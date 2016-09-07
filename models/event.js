d71b2f16a430f910a3902609fe7e66eba6d3070e
Ashlyn Aiu <ashlyn.aiu@gmail.com>
2016-09-06 21:04:48 -0700

    add type

--- models/event.js
+++ models/event.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose')
 var eventSchema = new mongoose.Schema({
   id: { type: Number, unique: true, index: true },
   occurrence_at: Date,
-  description: String
+  description: String,
+  type: String
 })
 
 module.exports = mongoose.model('Event', eventSchema)
